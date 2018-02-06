43ee113efb099d4efd79852eaa3bce072724ab63
mahao <351288465@qq.com>
2018-02-06 15:13:55 +0800

    Initial Commit

---
+++ collection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  collection
+//
+//  Created by 飞鱼 on 2018/2/6.
+//  Copyright © 2018年 马浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ collection/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  collection
+//
+//  Created by 飞鱼 on 2018/2/6.
+//  Copyright © 2018年 马浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
