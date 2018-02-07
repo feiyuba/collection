941d64d6cdbf51cacb38f963d02cebeb83344b9b
mahao <351288465@qq.com>
2018-02-07 11:23:12 +0800

    mh commit

---
+++ collection/CollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.h
+//  collection
+//
+//  Created by 飞鱼 on 2018/2/6.
+//  Copyright © 2018年 马浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewCell : UICollectionViewCell
+
+@end
---
+++ collection/CollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.m
+//  collection
+//
+//  Created by 飞鱼 on 2018/2/6.
+//  Copyright © 2018年 马浩. All rights reserved.
+//
+
+#import "CollectionViewCell.h"
+
+@implementation CollectionViewCell
+
+@end
